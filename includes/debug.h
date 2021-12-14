f94f104c8b8079f1dce84878009672fa35e81a79
Jonathan Michel <jonathanmichel@bluewin.ch>
2021-12-14 10:36:28 +0100

    Add debug level in print interface

--- includes/debug.h
+++ includes/debug.h
@@ -8,8 +8,8 @@
 
 #ifdef LEVEL_DEBUG
 
-#define DEBUG_PRINT(...) Serial.print(__VA_ARGS__); 
-#define DEBUG_PRINTLN(...) Serial.println(__VA_ARGS__); 
+#define DEBUG_PRINT(...) Serial.print("[DEBUG]"); Serial.print(__VA_ARGS__); 
+#define DEBUG_PRINTLN(...) Serial.print("[DEBUG]"); Serial.println(__VA_ARGS__); 
 
 #else
 
@@ -21,18 +21,18 @@
 // For now, we assume that info,
 // warning, error and fatal are always enabled
 
-#define INFO_PRINT(...) Serial.print(__VA_ARGS__); 
-#define INFO_PRINTLN(...) Serial.println(__VA_ARGS__); 
+#define INFO_PRINT(...) Serial.print("[INFO]"); Serial.print(__VA_ARGS__); 
+#define INFO_PRINTLN(...) Serial.print("[INFO]"); Serial.println(__VA_ARGS__); 
 
-#define WARNING_PRINT(...) Serial.print(__VA_ARGS__); 
-#define WARNING_PRINTLN(...) Serial.println(__VA_ARGS__); 
+#define WARNING_PRINT(...) Serial.print("[WARNING]"); Serial.print(__VA_ARGS__); 
+#define WARNING_PRINTLN(...) Serial.print("[WARNING]"); Serial.println(__VA_ARGS__); 
 
-#define ERROR_PRINT(...) Serial.print(__VA_ARGS__); 
-#define ERROR_PRINTLN(...) Serial.println(__VA_ARGS__); 
+#define ERROR_PRINT(...) Serial.print("[ERROR]"); Serial.print(__VA_ARGS__); 
+#define ERROR_PRINTLN(...) Serial.print("[ERROR]"); Serial.println(__VA_ARGS__); 
 
 // ! FATAL_PRINT does not exit code. This allows to print values before calling FATAL_PRINTLN
-#define FATAL_PRINT(...) Serial.print(__VA_ARGS__); 
-#define FATAL_PRINTLN(...) Serial.println(__VA_ARGS__); Serial.println("[FATAL] Exit code"); Serial.flush(); exit(0)
+#define FATAL_PRINT(...) Serial.print("[FATAL]"); Serial.print(__VA_ARGS__); 
+#define FATAL_PRINTLN(...) Serial.print("[FATAL]"); Serial.println(__VA_ARGS__); Serial.println("[FATAL] Exit code"); Serial.flush(); exit(0)
 
 void debugInit();
 
